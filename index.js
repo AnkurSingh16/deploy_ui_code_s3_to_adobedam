822a4b2d1157e05da543d13e9fcb2b8e0701ca39
Ankur Pawar <ankursingh2k14@gmail.com>
2020-06-18 16:59:10 +1000

    adding some code to ask for arguments

---
+++ index.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+const yargs = require("yargs");
+
+const options = yargs
+.usage("Usage: --zipversion <zip version>, --replicate <boolean>")
+.option("zver", {alias: "zipversion", describe: "provide explicit zip version to be deployed", type: "number", demandOption:false})
+.option("rep",{alias:"replicate",describe:"specify if you want aem replication to happen or not", type: "boolean", demandOption:true})
+// .default({zipversion: null, replicate:false})
+.argv;
+
+console.log(`your options are: zipversion = ${options.zipversion} and replicate=${options.replicate}!`)
